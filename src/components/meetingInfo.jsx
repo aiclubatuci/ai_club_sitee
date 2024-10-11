3637faa2b481f7a4dbd8491bd3e6e1ab851f8618
IanWearsHat <ianbbdai@gmail.com>
2024-10-10 19:11:42 -0700

    Made meeting info cards

--- src/components/meetingInfo.jsx
+++ src/components/meetingInfo.jsx
@@ -5,16 +5,20 @@ export const MeetingInfo = (props) => {
     <section id="meeting-info" className="text-center">
       <div className="container">
         <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Features</h2>
+          <h2>Meeting Info</h2>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
+                <div
+                  key={`${d.title}-${i}`}
+                  className="col-xs-12 col-md-6 outer-meeting-info-card"
+                >
+                  <div className="inner-meeting-info-card">
+                    <i className={d.icon}></i>
+                    <h3>{d.title}</h3>
+                    <p>{d.text}</p>
+                  </div>
                 </div>
               ))
             : "Loading..."}
