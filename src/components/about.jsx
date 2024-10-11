a11774dce04fa3edcb222401ea692619a227a40f
IanWearsHat <ianbbdai@gmail.com>
2024-10-10 18:44:07 -0700

    Changed about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,42 +2,39 @@ import React from "react";
 
 export const About = (props) => {
   return (
-    <div id="about">
+    <section id="about">
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
             <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
           </div>
+
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
+              <h2>We're AI@UCI.</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
+
+              <div>
+                {props.data
+                  ? props.data.Why.map((d, i) => (
+                      <div
+                        key={`${d[0]}-${i}`}
+                        className="col-lg-6 col-sm-6 col-xs-12 info-item"
+                      >
+                        <div>
+                          <h4>{d[0]}</h4>
+                          <span>{d[1]}</span>
+                        </div>
+                      </div>
+                    ))
+                  : "loading"}
               </div>
+
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
