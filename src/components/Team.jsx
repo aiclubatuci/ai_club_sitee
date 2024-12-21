ea2fe9ffe0def80e022e2cf49276a0f3f3f29f9f
Elise Ji <ccelisej.04@gmail.com>
2024-12-21 13:24:53 -0800

    deleted unnecessary code for header page

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -46,34 +46,4 @@ export const Team = (props) => {
     </div>
   );
 };
-/////////////////////////////////////////////////////
 
-{/* <div id="team" className="text-center">
-  <div className="container">
-    <div className="col-md-8 col-md-offset-2 section-title">
-      <h2>Meet the Board</h2>
-      <p>Our lovely team of twenty-six people!</p>
-    </div>
-    <div id="row">
-      {props.data
-        ? props.data.map((d, i) => (
-            <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-              <div className="thumbnail">
-                <div className="image-container">
-                  <img src={d.img} alt="..." className="team-img" />
-                  <div className="hover-info">
-                    <h4>{d.name}</h4>
-                    <p>{d.email}</p>
-                  </div>
-                </div>
-                <div className="caption">
-                  <h4>{d.name}</h4>
-                  <p>{d.job}</p>
-                </div>
-              </div>
-            </div>
-          ))
-        : "loading"}
-    </div>
-  </div>
-</div> */}
