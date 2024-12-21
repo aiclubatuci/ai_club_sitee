6fc4c694fc1b5879b728043aff73b59a7a1a063d
Elise Ji <ccelisej.04@gmail.com>
2024-12-21 13:04:30 -0800

    hyperlinked gmail and linkedin profile pages when you hover over the image. still need to get other board members gmail/linkedin info

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,6 +21,22 @@ export const Team = (props) => {
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
                     </div>
+
+                    <div className="hover-info">
+                    <p>
+                      {/* hyperlinked email/linkedin when u hover */}
+                      <a href={`mailto:${d.gmail}`} target="_blank" rel="noopener noreferrer">
+                        {d.gmail}
+                      </a>
+                    </p>
+                    <p>
+                      <a href={d.linkedin} target="_blank" rel="noopener noreferrer">
+                        LinkedIn Profile
+                      </a>
+                    </p>
+
+                  </div>
+
                   </div>
                 </div>
               ))
@@ -30,3 +46,34 @@ export const Team = (props) => {
     </div>
   );
 };
+/////////////////////////////////////////////////////
+
+{/* <div id="team" className="text-center">
+  <div className="container">
+    <div className="col-md-8 col-md-offset-2 section-title">
+      <h2>Meet the Board</h2>
+      <p>Our lovely team of twenty-six people!</p>
+    </div>
+    <div id="row">
+      {props.data
+        ? props.data.map((d, i) => (
+            <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+              <div className="thumbnail">
+                <div className="image-container">
+                  <img src={d.img} alt="..." className="team-img" />
+                  <div className="hover-info">
+                    <h4>{d.name}</h4>
+                    <p>{d.email}</p>
+                  </div>
+                </div>
+                <div className="caption">
+                  <h4>{d.name}</h4>
+                  <p>{d.job}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        : "loading"}
+    </div>
+  </div>
+</div> */}
