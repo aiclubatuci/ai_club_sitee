8b63789f629783fafa9f1b151dae4e9e3d824ce0
Elise Ji <ccelisej.04@gmail.com>
2024-12-21 14:24:43 -0800

    implemented advisor section. still have to implement spacing/formating

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -44,7 +44,7 @@ export const Navigation = (props) => {
             </li>
             <li>
               <a href="#testimonials" className="page-scroll">
-                Testimonials
+                Advisors
               </a>
             </li>
             <li>
--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -3,29 +3,40 @@ import React from "react";
 export const Testimonials = (props) => {
   return (
     <div id="testimonials">
-      <div className="container">
-        <div className="section-title text-center">
-          <h2>What our clients say</h2>
+        <div className="text-center">
+          <h2 className="advise-title">Advisors</h2>
         </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  <div className="testimonial">
-                    <div className="testimonial-image">
-                      {" "}
-                      <img src={d.img} alt="" />{" "}
-                    </div>
-                    <div className="testimonial-content">
-                      <p>"{d.text}"</p>
-                      <div className="testimonial-meta"> - {d.name} </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : "loading"}
+
+        <div className="advise-cont">
+          <div className="advise-box">
+            <div>
+              <img src="/img/advisors/ihler.png" alt="Globe" className="advise-pic"/>
+            </div>
+
+            <div>
+              <h1 className="advise-name">Alexander Ihler</h1>
+              <h3>"I work in artificial intelligence and machine learning, focusing on statistical methods for learning from data and on approximate inference techniques for graphical models. Applications of my work include data mining and information fusion in sensor networks, computer vision and image processing, and computational biology."</h3>
+            </div>
+
+          </div>
+
+
+          <div className="advise-box">
+            <div>
+              <img src="/img/advisors/mandt.png" alt="Globe" className="advise-pic"/>
+            </div>
+
+            <div>
+              <h1 className="advise-name">Stephan Mandt</h1>
+              <h3>"I work in artificial intelligence and machine learning, focusing on statistical methods for learning from data and on approximate inference techniques for graphical models. Applications of my work include data mining and information fusion in sensor networks, computer vision and image processing, and computational biology."</h3>
+            </div>
+            
+          </div>
+
         </div>
-      </div>
+        
+    
+    
     </div>
   );
 };
